7361bbfbcbb8983b42b9d16f68320f03b8e8ec41
alumno <alumno>
2019-02-06 20:24:24 -0300

    terminado

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,23 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var numerouno;
+	var numerodos;
+	var suma;
 
+
+
+	numerouno=numeroUno.value;
+	numerodos=numeroDos.value;
+
+	
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+
+	suma=numerouno+numerodos;
+
+	alert(suma);
+
+	alert("las suma es "+suma);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,89 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+	var numerouno;
+	var numerodos;
+	var suma;
+
+
+
+	numerouno=numeroUno.value;
+	numerodos=numeroDos.value;
+
 	
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+
+	suma=numerouno+numerodos;
+
+	
+
+	alert("las suma es "+suma);
 }
 
 function restar()
 {
+	var numerouno;
+	var numerodos;
+	var suma;
+
+
+
+	numerouno=numeroUno.value;
+	numerodos=numeroDos.value;
+
 	
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+
+	suma=numerouno-numerodos;
+
+	
+
+	alert("las resta es "+suma);
 }
 
 function multiplicar()
 { 
+	var numerouno;
+	var numerodos;
+	var suma;
+
+
+
+	numerouno=numeroUno.value;
+	numerodos=numeroDos.value;
+
 	
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+
+	suma=numerouno*numerodos;
+
+	
+
+	alert("las multiplicacion es "+suma);
 }
 
 function dividir()
 {
+	var numerouno;
+	var numerodos;
+	var suma;
+
+
+
+	numerouno=numeroUno.value;
+	numerodos=numeroDos.value;
+
 	
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+
+	suma=numerouno/numerodos;
+
+	
+
+	alert("las divicion es "+suma);
 }
 
