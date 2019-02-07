e45b83caca29a6309fda5c514138100affa8296f
alumno <alumno>
2019-02-06 21:55:11 -0300

    todo terminado

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var sueldos;
+	var aumento;
+
+	sueldos=importe.value;
+
+
+	sueldos=parseInt(sueldos);
+
+	aumento=(sueldos*25)/100;
+
+	resultado.value=sueldos-aumento;
+	alert(sueldos-aumento);
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,16 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	
+	var sueldos;
+	var aumento;
+
+	sueldos=sueldo.value;
+
+
+	sueldos=parseInt(sueldos);
+
+	aumento=(sueldos*10)/100;
+
+	resultado.value=aumento+sueldos;
+	alert(aumento+sueldos);
 }
