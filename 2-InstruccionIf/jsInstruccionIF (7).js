5c8f9db1012b786b8b3da1c59f90097afe1de12c
alumno <alumno>
2019-02-11 22:15:38 -0300

    falta terminar

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,22 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+	var edades;
+	var civil;
+
+edades=edad.value;
+
+edades=parseInt(edades);
+civil=estadoCivil.value;
+
+if (edades<18) 
+{
+	if () 
+	{
+		alert("Es muy pequeño para NO ser soltero.");
+	}
+}
+
 
 
 }//FIN DE LA FUNCIÓN
