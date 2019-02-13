1525cc4a4fc6bfa7f541501d3f5dc2ef41ea850f
alumno <alumno>
2019-02-13 19:27:39 -0300

    terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,14 +1,23 @@
 function mostrar()
 {
   
-	var localidad;
-	var nombre;
+	var precio;
+	var recargo;
+	var total;
 
-	localidad=laLocalidad.value;
-	nombre=elNombre.value;
+	//no hay recio asi que puso uno random entre 1 y 10.000
+	precio= alert(Math.floor((Math.random() * 10000) + 1));
+	recargo=(precio*10)/100;
 
-	alert("Usted es " + nombre + " y vive en la localidad de " + localidad);
+	total=precio+recargo;
+
+	
+	
 
 
+
+	alert("el precio del producto en efectivo es:$ " + precio +"con tarjeta tiene un recargo del 10% que seria" + recargo+" de recargo, costandole final$ "+ total );
+
+	
 
 }
