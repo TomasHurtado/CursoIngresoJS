5f934d9d1c690c531acca8558405a623a8301f37
TomasHurtado <tomas221298@gmail.com>
2019-02-20 13:27:10 -0300

    terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+	var planeta;
+	var mensaje="";
+	planeta=prompt("Ingrese un planeta");
 
+	switch(planeta)
+	{
+		case"tierra":
+		mensaje="aca vivimos";
+		break;
+		case"mercurio":
+		case"venus":
+		mensaje="aca hace mas calor";
+		break;
+		case"marte":
+		case"jupiter":
+		case"saturno":
+		case"urano":
+		case"neptuno":
+		
+		alert("acá hace más frio");
+		default:
+		mensaje="eso no es un planeta";
+	}
+	if (mensaje!="") 
+	{
+		alert(mensaje);
+	}
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,12 @@
 function mostrar()
 {
+	var hora;
 
+
+	hora=parseInt(laHora.value);
+
+	swicth(hora)
+	{
+		
+	}
 }
