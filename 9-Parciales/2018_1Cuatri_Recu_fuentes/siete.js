e21229bd036716bc7b1f5d5fd3dcdc6b8eb6661f
TomasHurtado <tomas221298@gmail.com>
2019-02-27 16:35:13 -0300

    terminado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,30 +1,48 @@
 function mostrar()
 {
-	var notas;
+	var promedio = 0;
+	var contador;
+	var ingreso;
 	var sexo;
-	var promedio;
-	var notabaja;
+	var cantidadVarones = 0;
+	var notaBaja = 11;
+	var sexoBajo;
 
-	
-			
-		for(c=0;c<5;c++)
-		{
-		while(notas<0 || notas>9) 
-            {
-            	notas=prompt("ingrese nota otra vez");
-            	c=c--;
-               
-               sexo = prompt("ingrese f ó m .");
+	for(contador = 0; contador < 5; contador++){
+		ingreso = prompt("Ingrese la nota");
+		ingreso = parseInt(ingreso);
 
+		while(isNaN(ingreso) || ingreso < 0 || ingreso > 10){
+			ingreso = prompt("Error, ingrese una nota valida");
+			ingreso = parseInt(ingreso);
+		}
+		
 
-				while(sexo!= "f" && sexo!="m")
-				{
-				sexo=prompt("intente otra vez");	
-				c=c--;
-				}
-				notas=prompt("ingrese nota");
-            }
-        }    
+		sexo = prompt("Ingrese el sexo")
+		while(sexo != "f" && sexo != "m"){
+			sexo = prompt("Error, ingrese un sexo valido. (m, f)");
+		}
 
+		promedio = promedio + ingreso; //sumo las notas
+
+		if(sexo == "m" && ingreso >= 6){ //cantidad de varones cuya nota sea mayor a 6
+			cantidadVarones = cantidadVarones + 1;
+		}
+		
+		if(ingreso < notaBaja){ //nota mas baja
+			notaBaja = ingreso;
+			sexoBajo = sexo;
+		}
+	}
+
+	if(sexoBajo == "m"){
+		sexoBajo = "masculino";
+	}else{
+		sexoBajo = "femenino;"
+	}
+
+	promedio = promedio / 5;
+	alert("El promedio es: " + promedio);
+	alert("La nota mas baja es " + notaBaja + " y el sexo del alumno es " + sexoBajo);
+	alert("Los cantidad de varones cuya nota es mayor o igual a 6 es de: " + cantidadVarones);
 }
- 
